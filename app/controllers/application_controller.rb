cabbc6ca3ab1851b3e1a2c5148d8068185f4f461
julirip <webdesign.hs@gmail.com>
2010-08-29 16:05:07 +0200

    added global application password authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,16 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+	USER_NAME, PASSWORD = "libsukes", "pertuis84"
+	before_filter :authenticate
+
+	private
+	  def authenticate
+	    authenticate_or_request_with_http_basic do |user_name, password|
+	      user_name == USER_NAME && password == PASSWORD
+	    end
+	  end
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
