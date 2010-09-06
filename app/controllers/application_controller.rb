b90e64d6a0170459adbbed766dc4649c4292d320
julirip <webdesign.hs@gmail.com>
2010-09-06 09:58:54 +0200

    added localization in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
 	USER_NAME, PASSWORD = "libsukes", "pertuis84"
-	before_filter :authenticate
+	before_filter :authenticate, :set_locale
 
 	private
 	  def authenticate
@@ -14,6 +14,25 @@ class ApplicationController < ActionController::Base
 	      user_name == USER_NAME && password == PASSWORD
 	    end
 	  end
+	
+	private
+		def set_locale
+			if params[:locale]
+				# if params[:locale] is nil then I18n.default_locale will be used
+				I18n.locale = params[:locale]
+				session[:locale] = params[:locale]
+			elsif session[:locale]
+				I18n.locale = session[:locale]
+			else
+				I18n.locale = extract_locale_from_accept_language_header
+				session[:locale] = params[:locale]
+			end
+		end
+		
+	private
+		def extract_locale_from_accept_language_header
+		  request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+		end
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
